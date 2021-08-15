cc5baad219946e08a0d29fe25f2e97f0034ffb01
Daniel Frąk <dan.frak@gmail.com>
2021-08-15 16:02:50 +0200

    More work on Settings side nav

--- frontend/src/main/angular/src/app/core/pages/settings/settings-layout/settings-side-nav/settings-side-nav.component.ts
+++ frontend/src/main/angular/src/app/core/pages/settings/settings-layout/settings-side-nav/settings-side-nav.component.ts
@@ -1,14 +1,15 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, HostBinding, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-settings-side-nav',
   templateUrl: './settings-side-nav.component.html',
-  styleUrls: ['./settings-side-nav.component.scss']
+  styleUrls: ['./settings-side-nav.component.scss'],
 })
 export class SettingsSideNavComponent implements OnInit {
 
   @Input()
-  hideSideNav: boolean = false;
+  @HostBinding('class.hidden')
+  minimizeSideNav: boolean = false;
 
   constructor() { }
 
