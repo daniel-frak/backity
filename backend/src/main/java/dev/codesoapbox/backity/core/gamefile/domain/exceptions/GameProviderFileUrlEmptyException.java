8f930026f3f714d82499740a6657269beb2a11b1
Daniel Frąk <dan.frak@gmail.com>
2025-05-05 14:42:00 +0200

    refactor: Remove circular dependency between GameProviderFileUrlEmptyException and GameFile

--- backend/src/main/java/dev/codesoapbox/backity/core/gamefile/domain/exceptions/GameProviderFileUrlEmptyException.java
+++ backend/src/main/java/dev/codesoapbox/backity/core/gamefile/domain/exceptions/GameProviderFileUrlEmptyException.java
@@ -1,13 +1,11 @@
 package dev.codesoapbox.backity.core.gamefile.domain.exceptions;
 
-import dev.codesoapbox.backity.core.gamefile.domain.GameFile;
 import dev.codesoapbox.backity.core.gamefile.domain.GameFileId;
 import dev.codesoapbox.backity.shared.domain.exceptions.DomainInvariantViolationException;
 
 public class GameProviderFileUrlEmptyException extends DomainInvariantViolationException {
 
     public GameProviderFileUrlEmptyException(GameFileId id) {
-        super("Game file url was null or empty for " + GameFile.class.getSimpleName()
-                + " with id: " + id.value());
+        super("Game file url was null or empty for Game File with id: " + id.value());
     }
 }
--- backend/src/test/java/dev/codesoapbox/backity/core/gamefile/domain/exceptions/GameProviderFileUrlEmptyExceptionTest.java
+++ backend/src/test/java/dev/codesoapbox/backity/core/gamefile/domain/exceptions/GameProviderFileUrlEmptyExceptionTest.java
@@ -14,7 +14,7 @@ class GameProviderFileUrlEmptyExceptionTest {
 
         String result = exception.getMessage();
 
-        var expectedResult = "Game file url was null or empty for GameFile with id: " +
+        var expectedResult = "Game file url was null or empty for Game File with id: " +
                              "acde26d7-33c7-42ee-be16-bca91a604b48";
         assertThat(result).isEqualTo(expectedResult);
     }
