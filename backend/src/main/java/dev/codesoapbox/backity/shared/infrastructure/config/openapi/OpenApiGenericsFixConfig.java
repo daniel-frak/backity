d6376aeed077ffb232aab96d7d271065bebd211b
Daniel Frąk <dan.frak@gmail.com>
2025-06-22 18:48:30 +0200

    feat: Update Springdoc version to 2.8.9

--- backend/src/main/java/dev/codesoapbox/backity/shared/infrastructure/config/openapi/OpenApiGenericsFixConfig.java
+++ backend/src/main/java/dev/codesoapbox/backity/shared/infrastructure/config/openapi/OpenApiGenericsFixConfig.java
@@ -1,14 +1,18 @@
 package dev.codesoapbox.backity.shared.infrastructure.config.openapi;
 
-import io.swagger.v3.core.converter.ModelConverters;
+import io.swagger.v3.core.converter.ModelConverter;
 import org.springdoc.core.providers.ObjectMapperProvider;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 
 @Configuration
 public class OpenApiGenericsFixConfig {
 
-    public OpenApiGenericsFixConfig(ObjectMapperProvider springDocObjectMapper) {
-        GenericsFixingModelConverter converter = new GenericsFixingModelConverter(springDocObjectMapper);
-        ModelConverters.getInstance().addConverter(converter);
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public ModelConverter genericsFixingModelConverter(ObjectMapperProvider omp) {
+        return new GenericsFixingModelConverter(omp);
     }
 }
