ed6832da74dc5d30fd4bdb1123373ac4199f96e0
Daniel Frąk <dan.frak@gmail.com>
2026-02-08 13:09:32 +0100

    feat(frontend): Remove getBadgeClass() in FileCopyStatusBadgeComponent

--- frontend/src/main/angular/src/app/core/components/file-copy-status-badge/file-copy-status-badge.component.spec.ts
+++ frontend/src/main/angular/src/app/core/components/file-copy-status-badge/file-copy-status-badge.component.spec.ts
@@ -1,7 +1,6 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {FileCopyStatusBadgeComponent} from './file-copy-status-badge.component';
-import {FileCopyStatus} from "@backend";
 
 describe('FileCopyStatusBadgeComponent', () => {
   let component: FileCopyStatusBadgeComponent;
@@ -29,47 +28,5 @@ describe('FileCopyStatusBadgeComponent', () => {
     fixture.detectChanges();
 
     expect(component.status()).toBe(undefined);
-  });
-
-  it('should return the correct badge class for "Untracked"', () => {
-    fixture.componentRef.setInput('status', undefined);
-    fixture.detectChanges();
-    expect(component.getBadgeClass()).toBe('bg-secondary');
-  });
-
-  it('should return the correct badge class for "Tracked"', () => {
-    fixture.componentRef.setInput('status', FileCopyStatus.Tracked);
-    fixture.detectChanges();
-    expect(component.getBadgeClass()).toBe('bg-secondary');
-  });
-
-  it('should return the correct badge class for "Stored (integrity unknown)"', () => {
-    fixture.componentRef.setInput('status', FileCopyStatus.StoredIntegrityUnknown);
-    fixture.detectChanges();
-    expect(component.getBadgeClass()).toBe('bg-success');
-  });
-
-  it('should return the correct badge class for "In Progress"', () => {
-    fixture.componentRef.setInput('status', FileCopyStatus.InProgress);
-    fixture.detectChanges();
-    expect(component.getBadgeClass()).toBe('bg-warning');
-  });
-
-  it('should return the correct badge class for "Failed"', () => {
-    fixture.componentRef.setInput('status', FileCopyStatus.Failed);
-    fixture.detectChanges();
-    expect(component.getBadgeClass()).toBe('bg-danger');
-  });
-
-  it('should return the correct badge class for "Enqueued"', () => {
-    fixture.componentRef.setInput('status', FileCopyStatus.Enqueued);
-    fixture.detectChanges();
-    expect(component.getBadgeClass()).toBe('bg-secondary');
-  });
-
-  it('should return the default badge class for undefined status', () => {
-    fixture.componentRef.setInput('status', undefined);
-    fixture.detectChanges();
-    expect(component.getBadgeClass()).toBe('bg-secondary');
   });
 });
