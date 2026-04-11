e3c0e6d3f3817c222bc058adc86ddadc4dd9fa11
Daniel Frąk <dan.frak@gmail.com>
2026-04-11 18:28:57 +0200

    test(e2e): Refactor BackendApi abstraction to favor composition over abstraction

--- e2e/src/test/java/dev/codesoapbox/backity/e2e/backend/BackupTargetsApi.java
+++ e2e/src/test/java/dev/codesoapbox/backity/e2e/backend/BackupTargetsApi.java
@@ -2,21 +2,19 @@ package dev.codesoapbox.backity.e2e.backend;
 
 import com.microsoft.playwright.Response;
 
-public class BackupTargetsApi extends BackendApi {
+public class BackupTargetsApi {
 
-    public BackupTargetsApi() {
-        super("/backup-targets");
-    }
+    private static final String URL = "/backup-targets";
 
     public boolean isCreated(Response response) {
-        return response.url().contains(url)
-                && response.request().method().equals("POST")
-                && isSuccessful(response);
+        return BackendApiAssertion.forResource(URL, response)
+                .isPost()
+                .isSuccessful();
     }
 
     public boolean isDeleted(Response response) {
-        return response.url().contains(url)
-                && response.request().method().equals("DELETE")
-                && isSuccessful(response);
+        return BackendApiAssertion.forResource(URL, response)
+                .isDelete()
+                .isSuccessful();
     }
 }
