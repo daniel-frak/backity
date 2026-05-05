ab17319110fa58db284ccfb288d106b587b7174b
Daniel Frąk <dan.frak@gmail.com>
2026-05-05 15:26:46 +0200

    refactor(backend): Extract WebSocket DTO value object mappers to shared classes

--- backend/src/main/java/dev/codesoapbox/backity/core/discovery/infrastructure/adapters/driven/messaging/ws/model/GameContentDiscoveryProgressChangedWsEventMapper.java
+++ backend/src/main/java/dev/codesoapbox/backity/core/discovery/infrastructure/adapters/driven/messaging/ws/model/GameContentDiscoveryProgressChangedWsEventMapper.java
@@ -1,6 +1,6 @@
 package dev.codesoapbox.backity.core.discovery.infrastructure.adapters.driven.messaging.ws.model;
 
-import dev.codesoapbox.backity.core.backup.infrastructure.adapters.driving.api.http.model.BackupValueObjectHttpDtoMapper;
+import dev.codesoapbox.backity.core.backup.infrastructure.adapters.driven.messaging.ws.model.BackupValueObjectWsDtoMapper;
 import dev.codesoapbox.backity.core.discovery.domain.events.GameContentDiscoveryProgressChangedEvent;
 import dev.codesoapbox.backity.shared.infrastructure.adapters.driven.messaging.ws.model.SharedWsDtoMapperConfig;
 import org.mapstruct.Mapper;
@@ -8,7 +8,7 @@ import org.mapstruct.Mapping;
 
 @Mapper(config = SharedWsDtoMapperConfig.class,
         uses = {
-                BackupValueObjectHttpDtoMapper.class
+                BackupValueObjectWsDtoMapper.class
         })
 public abstract class GameContentDiscoveryProgressChangedWsEventMapper {
 
