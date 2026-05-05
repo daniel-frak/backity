e48e4f02343b38f4125a3e1411f4b353ee5379d0
Daniel Frąk <dan.frak@gmail.com>
2026-05-05 11:02:44 +0200

    feat: Return 204 in all DELETE endpoints

--- backend/src/main/java/dev/codesoapbox/backity/core/filecopy/infrastructure/adapters/driving/api/http/controllers/DeleteFileCopyController.java
+++ backend/src/main/java/dev/codesoapbox/backity/core/filecopy/infrastructure/adapters/driving/api/http/controllers/DeleteFileCopyController.java
@@ -1,13 +1,14 @@
 package dev.codesoapbox.backity.core.filecopy.infrastructure.adapters.driving.api.http.controllers;
 
-import dev.codesoapbox.backity.core.filecopy.domain.FileCopyId;
 import dev.codesoapbox.backity.core.filecopy.application.usecases.DeleteFileCopyUseCase;
+import dev.codesoapbox.backity.core.filecopy.domain.FileCopyId;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @FileCopiesRestResource
 @RequiredArgsConstructor
@@ -18,12 +19,11 @@ public class DeleteFileCopyController {
 
     @Operation(summary = "Delete file copy", description = "Deletes an already replicated file copy")
     @DeleteMapping("{id}")
-    public ResponseEntity<Void> deleteFileCopy(
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteFileCopy(
             @SuppressWarnings("java:S6856")
             @PathVariable("id") String idValue) {
-        FileCopyId id = new FileCopyId(idValue);
+        var id = new FileCopyId(idValue);
         useCase.deleteFileCopy(id);
-
-        return ResponseEntity.noContent().build();
     }
 }
