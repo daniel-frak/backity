cfa12ae4e5418bb077eef947a960948c6e7d1bc8
Daniel Frąk <dan.frak@gmail.com>
2026-05-16 17:19:16 +0200

    chore(backend): Declare all modules as open to disable IDE warnings, remove unnecessary dependency

---
+++ backend/src/main/java/dev/codesoapbox/backity/shared/package-info.java
@@ -0,0 +1,7 @@
+/// Currently, Spring Modulith is only used for event outboxing,
+/// so all modules are declared open to disable IDE warnings.
+
+@ApplicationModule(type = ApplicationModule.Type.OPEN)
+package dev.codesoapbox.backity.shared;
+
+import org.springframework.modulith.ApplicationModule;
