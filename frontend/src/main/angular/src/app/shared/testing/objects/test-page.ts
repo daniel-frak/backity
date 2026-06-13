6bf98d906e3c526987aa0b3b1cd91b538ca3fd2e
Daniel Frąk <dan.frak@gmail.com>
2026-06-13 17:02:14 +0200

    refactor(frontend): Remove unnecessary assertion

--- frontend/src/main/angular/src/app/shared/testing/objects/test-page.ts
+++ frontend/src/main/angular/src/app/shared/testing/objects/test-page.ts
@@ -3,6 +3,6 @@ import {Page} from "@app/shared/components/table/page";
 export namespace TestPage {
 
   export function of<T>(content: T[]): Page<T> {
-    return {content: content} as Page<T>;
+    return {content: content};
   }
 }
